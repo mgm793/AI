5cd995f5634dd9b2f7cc091501011a70c4b39412
Marc Garcia <mgm793@ciutadella-146-57.eduroamcc.upf.edu>
2017-03-04 14:04:38 +0100

    ja funca

--- P1PLN/main.py
+++ P1PLN/main.py
@@ -9,93 +9,87 @@ def init():
 class SetLexic():
 
 	def __init__(self):
-		self.file = open("corpus.txt","r")
+		self.file = "corpus.txt"
 		self.exit = open("lexic.txt","w")
 		self.dicc = {}
-		self.lines = []
-		self.read()
+		self.lines = None
 		self.setDicc()
 		self.writeFile()
 
 	def getDicc(self):
 		return self.dicc
 
-	def read(self):
-		self.lines = self.file.readlines()
-		self.file.close()
 
 	def setDicc(self):
-		for line in self.lines:
-			line = line.decode("latin_1").encode("UTF-8")
-			line = line.replace("\r\n", "")
-			word, typo = line.split("\t")
-			word = word.lower()
-			try:
-				t = self.dicc[word]
+		with open(self.file) as f:
+			for line in f:
+				line = line.decode("latin_1").encode("UTF-8")
+				line = line.replace("\r\n", "")
+				word, typo = line.split("\t")
+				word = word.lower()
 				try:
-					t[typo] += 1
+					t = self.dicc[word]
+					try:
+						t[typo] += 1
+					except KeyError, err:
+						t[typo] = 1
 				except KeyError, err:
-					t[typo] = 1
-			except KeyError, err:
-				self.dicc[word] = {typo: 1}
+					self.dicc[word] = {typo: 1}
+		f.close()
 
 	def writeFile(self):
-		text = ""
 		for key in self.dicc:
 			for tag in self.dicc[key].keys():
-				text += key + "\t" + tag + "\t" + str(self.dicc[key][tag]) + "\n"
-		self.exit.write(text)
+				text = key + "\t" + tag + "\t" + str(self.dicc[key][tag]) + "\n"
+				self.exit.write(text)
 		self.exit.close()
 
 class Test():
 
 	def __init__(self,lexic,file):
 		self.dicc = lexic.getDicc()
-		self.file = open(file,"r")
+		self.file = file
 		self.res = open("res_" + file, "w")
-		self.lines = []
-		self.read()
+		self.lines = None
 		self.setRes()
 
-	def read(self):
-		self.lines = self.file.readlines()
-		self.file.close()
-
 	def setRes(self):
-		text = ""
-		for line in self.lines:
-			line = line.decode("latin_1").encode("UTF-8")
-			word = line.replace("\r\n", "").lower()
-			try:
-				types = self.dicc[word]
-				types = sorted(types.items(), key=lambda t: t[1], reverse=True)
-				text += word + "\t" + types[0][0] + "\n"
-			except KeyError, err:
-				text += word + "\t" + "NC" + "\n"
-		self.res.write(text)
+		with open(self.file) as f:
+			for line in f:
+				line = line.decode("latin_1").encode("UTF-8")
+				word = line.replace("\r\n", "").lower()
+				try:
+					types = self.dicc[word]
+					types = sorted(types.items(), key=lambda t: t[1], reverse=True)
+					text = word + "\t" + types[0][0] + "\n"
+				except KeyError, err:
+					text = word + "\t" + "NC" + "\n"
+				self.res.write(text)
+		f.close()
+		self.res.close()
 
 class Evaluate():
  
 	def __init__(self, test,gold):
 		self.testFile = open("res_" + test,"r")
 		self.solFile = open(gold,"r")
-		self.testLines = []
-		self.solLines = []
+		self.testLines = None
+		self.solLines = None
 		self.read()
 		print "Accuracy: %.2f" % self.calcAccuracy() + "%"
  
 	def read(self):
 		self.testLines = self.testFile.readlines()
-		self.testFile.close()
 		self.solLines = self.solFile.readlines()
 		self.solFile.close()
+		self.testFile.close()
    
 	def calcAccuracy(self):
 		ocNumber = len(self.solLines)
 		succesNumber = 0
 		for test in range(len(self.testLines)):
-			testWord , testTag = self.testLines[test].split("\t")
 			solWord , solTag = self.solLines[test].split("\t")
+			testWord , testTag = self.testLines[test].split("\t")
 			testTag = testTag.replace('\n','')
 			solTag 	=  solTag.replace('\r\n','')
 
