2a90ea6b243870266ef975541e953794fddf041c
Marc Garcia <mgm793@poblenou-139-96.eduroamcp.upf.edu>
2017-03-16 17:56:08 +0100

    funca

--- P2PLN/main.py
+++ P2PLN/main.py
@@ -8,8 +8,7 @@ path = './dataset/'
 def init():
 	n = raw_input("Number of most common words: ")
 	corpus = CreateCorpus(n)
-	print corpus.counts
-	# calcVect = CalcVect(corpus.counts)
+	calcVect = CalcVect(corpus.counts)
 
 class CreateCorpus():
 
@@ -27,6 +26,7 @@ class CreateCorpus():
 				self.counts = collections.Counter(words)
 			else:
 				self.counts += collections.Counter(words)
+			f.close()
 		self.counts = [str(word[0]) for word in self.counts.most_common(int(n))]
 			
 
@@ -34,16 +34,32 @@ class CalcVect():
 
 	def __init__(self, vect):
 		self.vector = None
+		self.readAndMake(vect)
 
 		
 	def readAndMake(self,vect):
+		output = open("output.arff","w")
+		output.write("@relation training\n\n")
+		for v in sorted(vect):
+			output.write("@attribute " + v + " numeric\n")
+		output.write("@attribute gender {male,female}\n\n@data\n")
 		for filename in os.listdir(path):
+			gender = filename.split("_")[1]
+			counts = []
 			f = codecs.open(path + filename,"r",encoding="utf-8")
 			text = f.read()
 			words = nltk.word_tokenize(text)
-			words = [word.lower() for word in words if word.isalpha() and word in vect]
-			counts = collections.Counter(words)
-
-
+			lenW = len(words)
+			words = [str(word.lower()) for word in words if word.isalpha() and word in vect]
+			counts += collections.Counter(words).most_common()
+			counts += [(word , 0) for word in vect if word not in words]
+			counts.sort(key=lambda x: x[0])
+			counts = [str(word[1]) for word in counts]
+			counts = [int(num) / float(lenW) for num in counts]
+			for c in counts:
+				output.write(str(c)+",")
+			output.write(gender+"\n")
+			f.close()
+		output.close()
 init()
 
