20c0d9dfb5dcf5858e4f64a2b1a295d40434bd38
Marc Garcia <mgm793@poblenou-133-87.eduroamcp.upf.edu>
2017-03-17 17:04:59 +0100

    weka 2

--- P2PLN/main.py
+++ P2PLN/main.py
@@ -2,7 +2,7 @@ from outputWeka import OutputWeka
 from most_common import Most_common
 
 def init():
-	path = './test/'
+	path = './dataset/'
 	n = raw_input("Number of most common words: ")
 	common = Most_common(n,path)
 	calcVect = OutputWeka(common.counts,n,path)
